d53d67bbe15c801a1eade549770b658be471dbdd
Tariq2518 <tariq.hussain2518@gmail.com>
2024-03-07 14:17:21 +0500

    configuration changes

--- app/src/main/java/com/poiesistech/lazyfcm/MainActivity.kt
+++ app/src/main/java/com/poiesistech/lazyfcm/MainActivity.kt
@@ -11,19 +11,6 @@
  *  **************************************************************
  */
 
-/*
- * **************************************************************
- *  * @Author: Tariq Hussain
- *  * @Date: 3/7/24, 11:52 AM.
- *  * @Accounts
- *  *      -> https://github.com/Tariq2518
- *  *      -> https://www.linkedin.com/in/tariqhpk/
- *  *
- *  * All rights reserved.
- *  * Copying and redistributing are not allowed.
- *  **************************************************************
- */
-
 package com.poiesistech.lazyfcm
 
 import androidx.appcompat.app.AppCompatActivity
