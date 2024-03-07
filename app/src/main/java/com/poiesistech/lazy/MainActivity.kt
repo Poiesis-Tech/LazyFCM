7606b6316cf9a729c446a630abd97aca9cfa7edc
Tariq2518 <tariq.hussain2518@gmail.com>
2024-03-07 11:52:13 +0500

    configuration changes

--- app/src/main/java/com/poiesistech/lazy/MainActivity.kt
+++ app/src/main/java/com/poiesistech/lazy/MainActivity.kt
@@ -1,13 +1,16 @@
-package com.poiesistech.lazyfcm
+package com.poiesistech.lazy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.poiesistech.lazyfcm.R
 import com.poiesistech.lazyfcm.fcm.LazyFCM
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         LazyFCM.setupFirebaseMessaging(this, packageName)
     }
 }
